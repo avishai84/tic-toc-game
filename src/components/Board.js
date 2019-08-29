dc2865cd2551adf3828355443bebc905ddc91370
avishai <avishai_taashur@gap.com>
2019-08-29 09:53:32 -0700

    adding callback to update state in Game

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,11 +5,20 @@ class Board extends Component{
     constructor(props){
         super(props);
         this.state = {
-            sqr: this.props.squares
+            sqr: this.props.squares,
+            nextTurnIs: this.props.nextTurnIs
         };
     }
     triggeredFromSquare(e){
-        console.log(e.target.id);
+        if(e.target.nodeName === 'BUTTON'){
+            console.log(e.target.id);
+            // target the btn which was clicked
+            this.setState({
+                nextTurnIs: !this.state.nextTurnIs
+            });
+           this.props.updateGame(this.state.nextTurnIs);
+        }
+
     }
 
     render(){
--- src/components/Game.js
+++ src/components/Game.js
@@ -6,15 +6,26 @@ class Game extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      square: Array(9).fill(null)
+      square: Array(9).fill(null),
+      nextTurnIs: true
     };
+
+  }
+   updateGame(state){
+   console.log('State: '+ state);
+    this.setState({
+   
+      nextTurnIs: state
+     
+    })
   }
   render(){
+    console.log(this.state.nextTurnIs);
     return (
       <div className="game">
         game
-        <Board squares={this.state.square} onClick={this.props}/>
-        <Status/>
+        <Board squares={this.state.square}  nextTurnIs={this.state.nextTurnIs} updateGame={this.updateGame.bind(this)}/>
+        <Status nextTurnIs={this.state.nextTurnIs}/>
       </div>
     );
   } 
