31d4f298d1316d3f4b199b11dd46867d9d6e1560
avishai <avishai_taashur@gap.com>
2019-09-01 12:49:18 -0700

    First test: testing if class not exist. should return faild

--- src/App.test.js
+++ src/App.test.js
@@ -8,21 +8,52 @@ import { expect } from 'chai';
 
 
 
+
+
+describe('testing class square exist', () => {
+  it('will check if .square class present', () => {
+    //const onButtonClick = sinon.spy();
+    //<Sqaure className="square" onClick={onButtonClick}></Sqaure>
+    const component = shallow(<Sqaure></Sqaure>);
+     expect(component.exists('.square')).to.equal(false);
+       console.log(component.debug());
+      // console.log(onButtonClick.debug());
+      //component.find('.square');
+      //component.find('.square').simulate('click');
+      //expect(component).to.have.property('');
+      //expect(onButtonClick).to.have.property('onClick');
+      //component.find('.square').to.have.property('button');
+
+
+  })
+})
+
+// describe('testing user click', () => {
+//   it('simulates click events', () => {
+//     const onButtonClick = sinon.spy();
+//     //<Sqaure className="square" onClick={onButtonClick}></Sqaure>
+//     const component = shallow(<Sqaure onClick={undefined}></Sqaure>);
+//      expect(component).to.have.properties('button');
+//        console.log(component.debug());
+      // console.log(onButtonClick.debug());
+      //component.find('.square');
+      //component.find('.square').simulate('click');
+      //expect(component).to.have.property('');
+      //expect(onButtonClick).to.have.property('onClick');
+      //component.find('.square').to.have.property('button');
+
+
+//   })
+// })
+//it('getting initial value from player', () =>{});
+
+
+
+
+
+
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
 //   ReactDOM.render(<App />, div);
 //   ReactDOM.unmountComponentAtNode(div);
 // });
-
-describe('testing user click', () => {
-  it('simulates click events', () => {
-    const onButtonClick = sinon.spy();
-    const component = shallow(<Sqaure className="square" ></Sqaure>);
-      // console.log(component.debug());
-      // console.log(onButtonClick.debug());
-      component.find('.square').simulate('click');
-      expect(onButtonClick).to.have.property('onClick');
-
-  })
-})
-it('getting initial value from player', () =>{});
